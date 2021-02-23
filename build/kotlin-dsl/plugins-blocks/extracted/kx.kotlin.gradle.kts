f1fb2d135ef58aa433a33971e1e8f21a982c23df
Giuseppe Barbieri <elect86@gmail.com>
2021-02-23 13:19:15 +0100

    up

--- build/kotlin-dsl/plugins-blocks/extracted/kx.kotlin.gradle.kts
+++
@@ -1,8 +0,0 @@
-
-plugins {
-    java
-    kotlin("jvm") version "1.4.30"
-    id("com.github.johnrengelman.shadow").version("6.1.0")
-                                                
-                
-}
