ee34ae1749a3d587a738610ac0664ac8d0c20c35
Giuseppe Barbieri <elect86@gmail.com>
2021-02-23 14:00:06 +0100

    up

--- src/main/kotlin/kx.kotlin.gradle.kts
+++ src/main/kotlin/kx.kotlin.gradle.kts
@@ -9,6 +9,7 @@ repositories {
 
 dependencies {
     implementation("org.jetbrains.kotlin.jvm:org.jetbrains.kotlin.jvm.gradle.plugin:1.4.30")
+    implementation("org.gradle.kotlin:gradle-kotlin-dsl-plugins:2.1.1")
 //    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.4.30")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:1.4.30")
     implementation("com.github.johnrengelman.shadow:com.github.johnrengelman.shadow.gradle.plugin:6.1.0")
