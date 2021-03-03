7047f8c506ce78cd9c53b522a04f8a91a25ff034
Giuseppe Barbieri <elect86@gmail.com>
2021-03-03 21:53:22 +0100

    up

---
+++ lwjgl/src/main/kotlin/com.github.elect86.build-logic.lwjgl.gradle.kts
@@ -0,0 +1,15 @@
+import gradle.kotlin.dsl.accessors._0bd82debbfba65caa505ce324b6ffa87.implementation
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.java
+
+plugins {
+    java
+}
+
+dependencies {
+
+    implementation(platform("kx.platform:source:0.0.1"))
+}
+
+
+
