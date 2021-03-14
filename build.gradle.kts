f12a4c7fb105a4bc86bf0ca92fb8c28215bf6128
Giuseppe Barbieri <elect86@gmail.com>
2021-03-14 02:14:09 +0100

    up

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,14 +28,15 @@ subprojects {
     }
 
     group = "kotlin.graphics.build-logic"
-    version = gitDescribe//"0.7.0"
+    version = "0.7.0"
 
     // limited dsl support inside here
     fun publishing(configure: Action<PublishingExtension>) = extensions.configure("publishing", configure)
 
     publishing {
         repositories.maven {
-            url = uri("$rootDir/../mary")
+//            url = uri("$rootDir/../mary")
+            url = uri("../mary")
         }
     }
 }
@@ -43,50 +44,50 @@ subprojects {
 
 
 
-tasks {
-    register("commit&push") {
-        group = "kx"
-        doLast {
-            rootProject.exec { commandLine("git", "add", ".") }
-            var message = gitDescribe.substringBeforeLast('-')
-            val commits = message.substringAfterLast('-').toInt() + 1
-            message = message.substringBeforeLast('-') + "-$commits"
-            rootProject.exec { commandLine("git", "commit", "-m", message) }
-            rootProject.exec { commandLine("git", "push") }
-        }
-    }
-    register("publishSnapshot") {
-        group = "kx"
-        doLast {
-            subprojects { version = gitDescribe }
-            println("publish")
-        }
-        dependsOn("commit&push")
-        finalizedBy(
-            subprojects.map { it.tasks.matching { task -> task.name == "publish" } },
-            "commit&pushMary"
-        )
-    }
-    register("commit&pushMary") {
-        group = "kx"
-        doLast {
-            val maryDir = file("$rootDir/../mary")
-            rootProject.exec {
-                workingDir = maryDir
-                commandLine("git", "add", ".")
-            }
-            val message = """
-                |$project :arrow_up:
-                |snapshot $gitDescribe
-            """.trimMargin()
-            rootProject.exec {
-                workingDir = maryDir
-                commandLine("git", "commit", "-m", message)
-            }
-            rootProject.exec {
-                workingDir = maryDir
-                commandLine("git", "push")
-            }
-        }
-    }
-}
+//tasks {
+//    register("commit&push") {
+//        group = "kx"
+//        doLast {
+//            rootProject.exec { commandLine("git", "add", ".") }
+//            var message = gitDescribe.substringBeforeLast('-')
+//            val commits = message.substringAfterLast('-').toInt() + 1
+//            message = message.substringBeforeLast('-') + "-$commits"
+//            rootProject.exec { commandLine("git", "commit", "-m", message) }
+//            rootProject.exec { commandLine("git", "push") }
+//        }
+//    }
+//    register("publishSnapshot") {
+//        group = "kx"
+//        doLast {
+//            subprojects { version = gitDescribe }
+//            println("publish")
+//        }
+//        dependsOn("commit&push")
+//        finalizedBy(
+//            subprojects.map { it.tasks.matching { task -> task.name == "publish" } },
+//            "commit&pushMary"
+//        )
+//    }
+//    register("commit&pushMary") {
+//        group = "kx"
+//        doLast {
+//            val maryDir = file("$rootDir/../mary")
+//            rootProject.exec {
+//                workingDir = maryDir
+//                commandLine("git", "add", ".")
+//            }
+//            val message = """
+//                |$project :arrow_up:
+//                |snapshot $gitDescribe
+//            """.trimMargin()
+//            rootProject.exec {
+//                workingDir = maryDir
+//                commandLine("git", "commit", "-m", message)
+//            }
+//            rootProject.exec {
+//                workingDir = maryDir
+//                commandLine("git", "push")
+//            }
+//        }
+//    }
+//}
