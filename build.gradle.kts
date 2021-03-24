dd2acdae2df70d5e63c53482285110a9d64e05a6
Giuseppe Barbieri <elect86@gmail.com>
2021-03-24 18:54:42 +0100

    v0.7.0+70

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,16 @@
-import java.io.ByteArrayOutputStream
-
 plugins {
     `kotlin-dsl` apply false
-    id("kx.snapshot") version "0.0.3"
+    id("kx.snapshot") version "0.0.4"
 }
 
-version = "0.7.0+69" // for ::bump
+version = "0.7.0+70" // for ::bump
 ext["platformVersion"] = "0.2.8+29"
 
 subprojects {
 
     group = "kotlin.graphics.build-logic"
     version = when (name) {
-        "dev" -> "0.0.3"
+        "dev" -> "0.0.4"
         else -> rootProject.version
     }
 
@@ -34,49 +32,3 @@ subprojects {
         }
     }
 }
-
-//val gitDescribe: String
-//    get() = ByteArrayOutputStream().also { exec { commandLine("git", "describe", "--tags"); standardOutput = it; } }.toString().trim()
-//
-//val gitDistance: Int
-//    get() = gitDescribe.substringBeforeLast("-g").substringAfterLast('-').toInt() + 1 // the next is the one we are interested in
-//
-//val gitTag: String
-//    get() = gitDescribe.substringBeforeLast('-').substringBeforeLast('-')
-//
-//fun gitAddCommitPush(message: String, dir: File = rootDir) {
-//    exec { workingDir = dir; commandLine("git", "add", "."); }
-//    exec { workingDir = dir; commandLine("git", "commit", "-m", message); }
-//    exec { workingDir = dir; commandLine("git", "push"); }
-//}
-//
-//tasks {
-//    register("1)bump,commit,push") {
-//        group = "kx-dev"
-//        doLast {
-//            bump()
-//            gitAddCommitPush("$gitTag+$gitDistance")
-//        }
-//    }
-//    register("2)publish") {
-//        group = "kx-dev"
-//        //        dependsOn("commit&push") not reliable
-//        finalizedBy(getTasksByName("publish", true) - named(":dev:publish"))
-//    }
-//    register("3)[mary]commit,push") {
-//        group = "kx-dev"
-//        doLast {
-//            gitAddCommitPush("""
-//                |$project :arrow_up:
-//                |snapshot $gitDescribe""".trimMargin(), file("../mary"))
-//        }
-//        //        mustRunAfter("publishSnapshot") // order
-//    }
-//}
-//
-//fun bump() {
-//    val text = buildFile.readText()
-//    val version = version.toString()
-//    val bump = "${version.split('+').first()}+%02d".format(gitDistance)
-//    buildFile.writeText(text.replace(version, bump))
-//}
